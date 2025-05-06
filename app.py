19cf7cecda4e528d06b6de2f89d43782de918d4f
Gabriele <gabriele.melucci@gmail.com>
2025-05-06 21:26:03 +0200

    fixed app.py

--- app.py
+++ app.py
@@ -9,9 +9,29 @@ app.register_blueprint(search_bp, url_prefix='/api')
 def home():
     return render_template('sitechinese.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/changelog')
+def changelog():
+    return render_template('changelog.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/privacy-policy')
+def privacy():
+    return render_template('privacy-policy.html')
+
 @app.route('/search')
 def search():
     return render_template('search.html')
+
+@app.route('/terms-of-use')
+def terms():
+    return render_template('terms-of-use.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
